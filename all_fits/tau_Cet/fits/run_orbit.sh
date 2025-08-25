e05fadb3732f368bf8cf216056b37e83d7e4211b
Sarah Blunt <sarah.blunt.3@gmail.com>
2025-08-25 15:54:33 -0700

    tau ceti fit script even though killed

--- all_fits/tau_Cet/fits/run_orbit.sh
+++ all_fits/tau_Cet/fits/run_orbit.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
-
-radvel fit -s 10700_radvel.py
-radvel mcmc -s 10700_radvel.py --nwalkers=150
-radvel derive -s 10700_radvel.py 
-radvel plot -t rv derived corner trend -s 10700_radvel.py
-radvel ic -t nplanets e trend jit -s 10700_radvel.py
-# radvel report -s 10700_radvel.py
+radvel fit -s 10700_radvel.py -d results
+radvel plot -t rv -s 10700_radvel.py -d results
+radvel mcmc -s 10700_radvel.py -d results
+radvel derive -s 10700_radvel.py -d results
+radvel plot -t rv trend derived corner -s 10700_radvel.py -d results
