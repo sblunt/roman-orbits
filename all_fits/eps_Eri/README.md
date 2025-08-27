1e69fb4eeb5de47283a92d3356bcb02f0d63d041
Sarah Blunt <sarah.blunt.3@gmail.com>
2025-08-26 17:21:03 -0700

    eps eri fit ready

--- all_fits/eps_Eri/README.md
+++ all_fits/eps_Eri/README.md
@@ -1,3 +1,5 @@
+Trimmed first 60% of chains.
+
 Parameters totally consistent with Thompson+ 2025. Honestly no need for a GP. Binning
 to 1 day is sufficient. Seems like it makes sense looking at Thompson+ fig 3-- the 
 noise prediction looks quite white on long timescale.
--- all_fits/eps_Eri/fits/run_orbit.sh
+++ all_fits/eps_Eri/fits/run_orbit.sh
@@ -1,5 +1,5 @@
 radvel fit -s 22049_radvel.py -d results
 radvel plot -t rv -s 22049_radvel.py -d results
-radvel mcmc -s 22049_radvel.py -d results --maxGR 1.001
+radvel mcmc -s 22049_radvel.py -d results --maxGR 1.003
 radvel derive -s 22049_radvel.py -d results
 radvel plot -t rv trend derived corner -s 22049_radvel.py -d results
