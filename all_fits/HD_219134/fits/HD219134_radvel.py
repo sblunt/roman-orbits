60db66031f7368d402a59201d68a2965d0a84b0e
Sarah Blunt <sarah.blunt.3@gmail.com>
2025-08-27 14:54:02 -0700

    219134 fit in progress

--- all_fits/HD_219134/fits/HD219134_radvel.py
+++ all_fits/HD_219134/fits/HD219134_radvel.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import radvel
+from radvel.orbit import timeperi_to_timetrans
 
 # Load Data
 data = pd.read_csv("../data/all_rvs_binned.csv")
@@ -23,50 +24,58 @@ planet_letters = {
 
 anybasis_params = radvel.Parameters(
     nplanets,
-    basis = 'per tp e w k',
+    basis = 'per tc e w k',
 )
 
 # https://exoplanetarchive.ipac.caltech.edu/overview/HD%20219134
 
-# Parameters retrieved from the Rosenthal 2021 fit unless otherwise noted
-# b
-anybasis_params['per1'] = radvel.Parameter(value = 3.092947)
-anybasis_params['tp1'] = radvel.Parameter(value = 2449999.5) # Vogt 2015 fit
-anybasis_params['e1'] = radvel.Parameter(value = 0.0630)
-anybasis_params['w1'] = radvel.Parameter(value = 9.70 * np.pi / 180.)
-anybasis_params['k1'] = radvel.Parameter(value = 2.13)
+#
+# b: 
+anybasis_params['per1'] = radvel.Parameter(value = 3.092920) # Seager et al 21
+anybasis_params['tc1'] = radvel.Parameter(value = 2458765.95501) # Seager et al 21
+anybasis_params['e1'] = radvel.Parameter(value = 0.06) # Rosenthal+ 21
+anybasis_params['w1'] = radvel.Parameter(value = 9.7 * np.pi/180) # Rosenthal+ 21
+anybasis_params['k1'] = radvel.Parameter(value = 1.9) # Rosenthal+ 21
 
-# c
-anybasis_params['per2'] = radvel.Parameter(value = 6.76406)
-anybasis_params['tp2'] = radvel.Parameter(value = 2449998.5) # Vogt 2015 fit
-anybasis_params['e2'] = radvel.Parameter(value = 0.16)
-anybasis_params['w2'] = radvel.Parameter(value = 6.30 * np.pi / 180.)
-anybasis_params['k2'] = radvel.Parameter(value = 1.44)
+# c: Seager et al 21
+anybasis_params['per2'] = radvel.Parameter(value = 6.765149) # Seager et al 21
+anybasis_params['tc2'] = radvel.Parameter(value = 2458766.16927) # Seager et al 21
+anybasis_params['e2'] = radvel.Parameter(value = 0.16) # Rosenthal+ 21
+anybasis_params['w2'] = radvel.Parameter(value = 6.30 * np.pi / 180.) # Rosenthal+ 21
+anybasis_params['k2'] = radvel.Parameter(value = 1.44) # Rosenthal+ 21
 
-# d
-anybasis_params['per3'] = radvel.Parameter(value = 46.734)
-anybasis_params['tp3'] = radvel.Parameter(value = 2449964.) # Vogt 2015 fit
-anybasis_params['e3'] = radvel.Parameter(value = 0.0770)
-anybasis_params['w3'] = radvel.Parameter(value = 340. * np.pi / 180.)
-anybasis_params['k3'] = radvel.Parameter(value = 3.43)
-
-# f: This is the controversial planet
-anybasis_params['per4'] = radvel.Parameter(value = 22.7945)
-anybasis_params['tp4'] = radvel.Parameter(value = 2449983.) # Vogt 2015 fit
-anybasis_params['e4'] = radvel.Parameter(value = 0.0720)
+# f: This is the controversial planet (params from Vogt+ 15): controversy
+# comes from identical period in S-indices (reported by Johsnon+ 2016).
+# I (Sarah) think it's fine to fit for this even if it is stellar activity. 
+# Could stand to think about this more, but I doubt it affects the outer planet
+# params
+tc4 = timeperi_to_timetrans(2449983, 22.805, 0.0720,  10. * np.pi / 180.)
+anybasis_params['per4'] = radvel.Parameter(value = 22.805)
+anybasis_params['tc4'] = radvel.Parameter(value = tc4)
+anybasis_params['e4'] = radvel.Parameter(value = 0.01)
 anybasis_params['w4'] = radvel.Parameter(value = 10. * np.pi / 180.)
-anybasis_params['k4'] = radvel.Parameter(value = 2.05)
+anybasis_params['k4'] = radvel.Parameter(value = 2.3)
 
-# g: All parameters from Vogt 2015
+# d: Vogt+ 15
+tc3 = timeperi_to_timetrans(2449964, 46.71, 0, 0)
+anybasis_params['per3'] = radvel.Parameter(value = 46.71)
+anybasis_params['tc3'] = radvel.Parameter(value = tc3)
+anybasis_params['e3'] = radvel.Parameter(value = 0.01)
+anybasis_params['w3'] = radvel.Parameter(value = 10. * np.pi / 180.)
+anybasis_params['k3'] = radvel.Parameter(value = 4.4)
+
+# g: Vogt+ 15
+tc5 = timeperi_to_timetrans(2449972, 94.2, 0,0)
 anybasis_params['per5'] = radvel.Parameter(value = 94.2)
-anybasis_params['tp5'] = radvel.Parameter(value = 2449972.)
-anybasis_params['e5'] = radvel.Parameter(value = 0.)
-anybasis_params['w5'] = radvel.Parameter(value = 0. * np.pi / 180.)
+anybasis_params['tc5'] = radvel.Parameter(value = tc5)
+anybasis_params['e5'] = radvel.Parameter(value = 0.01)
+anybasis_params['w5'] = radvel.Parameter(value = 10 * np.pi / 180.)
 anybasis_params['k5'] = radvel.Parameter(value = 1.8)
 
-# h
-anybasis_params['per6'] = radvel.Parameter(value = 2104.)
-anybasis_params['tp6'] = radvel.Parameter(value = 2448725.) # Vogt 2015 fit
+# h: Vogt+ 15
+tc6 = timeperi_to_timetrans(2448725., 22047, 0, 0)
+anybasis_params['per6'] = radvel.Parameter(value = 2247.)
+anybasis_params['tc6'] = radvel.Parameter(value = tc6)
 anybasis_params['e6'] = radvel.Parameter(value = 0.025)
 anybasis_params['w6'] = radvel.Parameter(value = 0.0 * np.pi / 180.)
 anybasis_params['k6'] = radvel.Parameter(value = 5.73)
@@ -74,11 +83,12 @@ anybasis_params['k6'] = radvel.Parameter(value = 5.73)
 anybasis_params['dvdt'] = radvel.Parameter(value = 0.0)
 anybasis_params['curv'] = radvel.Parameter(value = 0.0)
 
-# Adding instrument rv offset and jitter parameters (assuming 5 m/s for all for now)
+# Adding instrument rv offset and jitter parameters
 for inst in instnames:
-    anybasis_params['gamma_{}'.format(inst)] = radvel.Parameter(value = 0)
+    anybasis_params['gamma_{}'.format(inst)] = radvel.Parameter(value = np.mean(data.mnvel[data.tel == inst]))
 for inst in instnames:
     anybasis_params['jit_{}'.format(inst)] = radvel.Parameter(value = 5)
+
 
 params = anybasis_params.basis.to_any_basis(anybasis_params, fitting_basis)
 
@@ -88,6 +98,10 @@ params['curv'].vary = False
 priors = [
     radvel.prior.EccentricityPrior(nplanets),
     radvel.prior.PositiveKPrior(nplanets),
+    radvel.prior.Gaussian('per1', 3.092920,0.000011),
+    radvel.prior.Gaussian('tc1', 2458765.95501, 0.00047),
+    radvel.prior.Gaussian('per2',6.765149, 0.000036),
+    radvel.prior.Gaussian('tc2',2458766.16927, 0.00069)
 ]
 # Adding instrument-specific hardbounds priors for jitter
 for inst in instnames:
