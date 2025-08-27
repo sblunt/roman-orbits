0edc5c7006c98e6585026054654d31e1ee57ae5e
Sarah Blunt <sarah.blunt.3@gmail.com>
2025-08-27 14:28:03 -0700

    move to total # of steps radvel runs

--- all_fits/HD_160691/fits/run_orbit.sh
+++ all_fits/HD_160691/fits/run_orbit.sh
@@ -1,5 +1,5 @@
 radvel fit -s HD160691_radvel.py -d results
 radvel plot -t rv -s HD160691_radvel.py -d results
-# radvel mcmc -s HD160691_radvel.py -d results
-# radvel derive -s HD160691_radvel.py -d results
-# radvel plot -t rv trend derived corner -s HD160691_radvel.py -d results
+radvel mcmc -s HD160691_radvel.py -d results --nsteps 10000
+radvel derive -s HD160691_radvel.py -d results
+radvel plot -t rv trend derived corner -s HD160691_radvel.py -d results
