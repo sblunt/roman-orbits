87c7ee32c18bb17ecc058cc9709a50ca9d65ac4e
manduhmia <79128537+manduhmia@users.noreply.github.com>
2025-08-28 10:56:53 -0500

    Updating the HD 114783 data directory with a DataSetup.py and IAD + gost data files (HGCA fit)

--- all_fits/HD_114783/data/HD114783_DataSetup.py
+++ all_fits/HD_114783/data/HD114783_DataSetup.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import radvel
-import numpy as np
 import os
 
-# assumes the CLS and SOPHIE data files are in the same directory as this file
+# assumes the CLS and HARPS data files are in the same directory as this file
 this_dir = os.path.dirname(__file__)
 path1 = os.path.join(this_dir, "CLS_rv_raw.txt")
-path2 = os.path.join(this_dir, "SOPHIE_rv_raw.txt")
+path2 = os.path.join(this_dir, "HARPS_rv_raw.txt")
 
 # set nightly binning option
 binning = 1  # nightly bin, 1 for yes, 2 for no
@@ -31,39 +30,29 @@ data1_post["tel"] = "HIRES-post"
 data1_apf = data1[data1["tel"] == "apf"]  # APF Levy data
 data1_apf["tel"] = "APF"
 
-
-###############
-# SOPHIE data #
-###############
+#################
+# HARPS pre and #
+#   post data   #
+#################
 data2 = pd.read_csv(
-    path2,
+    path2, 
     header=None,
-    skiprows=17,
-    delim_whitespace=True,
-    usecols=[2,3,6,7],
-    names=('mnvel', 'errvel', 'mjd', 'time'),
+    skiprows=2,
+    sep='\s+',
+    usecols=[3,4,5],
+    names=('time','mnvel','errvel')
 )
-# convert mjd to bjd for observations with nan bjd
-for i, (bjd, mjd) in enumerate(zip(data2['time'].values, data2['mjd'].values)):
-     if np.isnan(bjd):
-         data2['time'].iloc[i] = mjd + 2400000
-# drop unneccesary labels
-data2 = data2.drop(columns='mjd')
-# drop observations with nan errvel
-data2 = data2.drop(index=np.where(np.isnan(data2['errvel']))[0])
-# convert RV measurements in km/s to m/s
-data2["mnvel"] *= 1000  
-data2["errvel"] *= 1000 
-# add instrument name 
-data2["tel"] = "SOPHIE"
+data2_pre = data2[data2['time'] < 2457174.5]   # pre-upgrade data
+data2_pre['tel'] = 'HARPS-pre'
+data2_post = data2[data2['time'] > 2457174.5]   # post-upgrade data
+data2_post['tel'] = 'HARPS-post'
 
 
-########################################################################################
 # data concatenation and optionally do nightly bin of all data sources
-dataframe = [data2, data1_ham, data1_pre, data1_post, data1_apf]
+dataframe = [data1_ham, data1_pre, data1_post, data1_apf, data2_pre, data2_post]
 data_all = pd.concat(dataframe, ignore_index=True)
 data_all.to_csv(
-    "HD95735_rv_combined_unbinned.txt",
+    "HD114783_rv_combined_unbinned.txt",
     sep="\t",
     index=False,
     header=True,
@@ -81,5 +70,5 @@ if binning == 1:
 
     data_all_bin = pd.DataFrame(data=bin_dict)
     data_all_bin.to_csv(
-        "HD95735_rv_combined_binned.txt", sep="\t", index=False, header=True
+        "HD114783_rv_combined_binned.txt", sep="\t", index=False, header=True
     )
